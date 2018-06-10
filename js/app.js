5be06b355a39ea8a13de2271e671f3535727d328
Annalise <awissler@uw.edu>
2018-06-10 14:37:12 -0700

    replace table with content

--- js/app.js
+++ js/app.js
@@ -2,21 +2,10 @@ function renderRandomQuote(QUOTES) {
   var tbody = document.querySelector("tbody");
   for (var i = 0; i < QUOTES.length; i++) {
     var quote = QUOTES[i];
-    tbody.appendChild(renderQuote(quote));
+    var quoteContent = document.createTextNode(quote.content + " ~" + quote.title);
+    tbody.appendChild(quoteContent);
   }
   return tbody;
-}
-
-function renderQuote(quote) {
-  var tr = document.createElement("tr");
-  tr.appendChild(renderQuoteProp(quote.content + " ~" + quote.title));
-  return tr;
-}
-
-function renderQuoteProp(content) {
-  var td = document.createElement("td");
-  td.textContent = content;
-  return td;
 }
 
 var quoteOutput = document.getElementById("quote-output");
