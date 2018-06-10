5352dadacef7e462a87ba8abd5268871a78742e6
Annalise <awissler@uw.edu>
2018-06-10 14:57:03 -0700

    try new code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
+
+var quoteOutput = document.getElementById("quote-output");
+var generateButton = document.getElementById("generate");
+
 function renderRandomQuote(QUOTES) {
   var tbody = document.querySelector("tbody");
   for (var i = 0; i < QUOTES.length; i++) {
     var quote = QUOTES[i];
     var quoteContent = document.createTextNode(quote.content + " ~" + quote.title);
     tbody.appendChild(quoteContent);
+    return QUOTES[Math.floor(Math.random() * QUOTES.length)];
   }
   return tbody;
 }
 
-var quoteOutput = document.getElementById("quote-output");
-var generateButton = document.getElementById("generate");
-
 generateButton.addEventListener("click", function() {
   quoteOutput.textContent = "";
-  QUOTES.length = 1;
   quoteOutput.textContent = renderRandomQuote(QUOTES);
 });
